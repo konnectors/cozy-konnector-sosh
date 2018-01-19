bb5513dfac053ea3b6ffd526e335183ad713f230
Travis CI User <travis@example.org>
2018-01-19 17:55:43 +0000

    publish: Merge 7dab5af6070b893ff106a316a98c00f9aefc61c3 into c1902e2a926c29b00de4710bcb8c9a54f2dc7e32
    generated from commit d862724e093fbf71e8ce485dd8089d2dbc888db0

--- index.js
+++ index.js
@@ -25721,8 +25721,8 @@ const getDateRangeFromBill = (bill, options) => {
   const date = getOperationDateFromBill(bill, options)
 
   return {
-    minDate: subDays(date, options.minDateDelta),
-    maxDate: addDays(date, options.maxDateDelta)
+    minDate: subDays(date, options.pastWindow),
+    maxDate: addDays(date, options.futureWindow)
   }
 }
 
@@ -55406,7 +55406,8 @@ const { findDebitOperation, findCreditOperation } = __webpack_require__(952)
 
 const DOCTYPE_OPERATIONS = 'io.cozy.bank.operations'
 const DEFAULT_AMOUNT_DELTA = 0.001
-const DEFAULT_DATE_DELTA = 15
+const DEFAULT_PAST_WINDOW = 15
+const DEFAULT_FUTURE_WINDOW = 29
 
 class Linker {
   constructor (cozyClient) {
@@ -55535,9 +55536,8 @@ module.exports = (bills, doctype, fields, options = {}) => {
   options.minAmountDelta = options.minAmountDelta || options.amountDelta
   options.maxAmountDelta = options.maxAmountDelta || options.amountDelta
 
-  options.dateDelta = options.dateDelta || DEFAULT_DATE_DELTA
-  options.minDateDelta = options.minDateDelta || options.dateDelta
-  options.maxDateDelta = options.maxDateDelta || options.dateDelta
+  options.pastWindow = options.pastWindow || DEFAULT_PAST_WINDOW
+  options.futureWindow = options.futureWindow || DEFAULT_FUTURE_WINDOW
 
   const cozyClient = __webpack_require__(52)
   const linker = new Linker(cozyClient)
