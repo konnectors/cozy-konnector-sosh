19239809b425b63690d80c0d90cdd912e872cb56
Travis CI User <travis@example.org>
2023-05-25 14:56:36 +0000

    publish: Merge pull request #573 from konnectors/moreBillsButton
    generated from commit fa2b89d65554be5912361497e2f20c274ff63db5

--- main.js
+++ main.js
@@ -5250,10 +5250,13 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
         throw new Error('VENDOR_DOWN')
       }
       let recentPdfNumber = await this.runInWorker('getPdfNumber')
-      await this.clickAndWait(
-        '[data-e2e="bh-more-bills"]',
-        '[aria-labelledby="bp-historicBillsHistoryTitle"]'
-      )
+      const hasMoreBills = await this.runInWorker('getMoreBillsButton')
+      if (hasMoreBills) {
+        await this.clickAndWait(
+          '[data-e2e="bh-more-bills"]',
+          '[aria-labelledby="bp-historicBillsHistoryTitle"]'
+        )
+      }
       let allPdfNumber = await this.runInWorker('getPdfNumber')
       let oldPdfNumber = allPdfNumber - recentPdfNumber
       for (let i = 0; i < recentPdfNumber; i++) {
@@ -5280,29 +5283,31 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
         )
       }
       this.log('debug', 'recentPdf loop ended')
-      for (let i = 0; i < oldPdfNumber; i++) {
-        this.log('debug', 'fetching ' + (i + 1) + '/' + oldPdfNumber)
-        // Same as above with the red frame, but for old bills board
-        const redFrame = await this.runInWorker('checkOldBillsRedFrame')
-        if (redFrame !== null) {
-          this.log('debug', 'Something went wrong during old pdfs loading')
-          throw new Error('VENDOR_DOWN')
+      if (oldPdfNumber != 0) {
+        for (let i = 0; i < oldPdfNumber; i++) {
+          this.log('debug', 'fetching ' + (i + 1) + '/' + oldPdfNumber)
+          // Same as above with the red frame, but for old bills board
+          const redFrame = await this.runInWorker('checkOldBillsRedFrame')
+          if (redFrame !== null) {
+            this.log('debug', 'Something went wrong during old pdfs loading')
+            throw new Error('VENDOR_DOWN')
+          }
+          await this.runInWorker('waitForOldPdfClicked', i)
+          await this.clickAndWait(
+            'a[class="o-link"]',
+            '[data-e2e="bp-tile-historic"]'
+          )
+          await this.clickAndWait(
+            '[data-e2e="bp-tile-historic"]',
+            '[aria-labelledby="bp-billsHistoryTitle"]'
+          )
+          await this.clickAndWait(
+            '[data-e2e="bh-more-bills"]',
+            '[aria-labelledby="bp-historicBillsHistoryTitle"]'
+          )
         }
-        await this.runInWorker('waitForOldPdfClicked', i)
-        await this.clickAndWait(
-          'a[class="o-link"]',
-          '[data-e2e="bp-tile-historic"]'
-        )
-        await this.clickAndWait(
-          '[data-e2e="bp-tile-historic"]',
-          '[aria-labelledby="bp-billsHistoryTitle"]'
-        )
-        await this.clickAndWait(
-          '[data-e2e="bh-more-bills"]',
-          '[aria-labelledby="bp-historicBillsHistoryTitle"]'
-        )
+        this.log('debug', 'oldPdf loop ended')
       }
-      this.log('debug', 'oldPdf loop ended')
       this.log('debug', 'pdfButtons all clicked')
       await this.runInWorker('processingBills')
       this.store.dataUri = []
@@ -5381,11 +5386,9 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
 
   findMoreBillsButton() {
     this.log('debug', 'Starting findMoreBillsButton')
-    const button = Array.from(
-      document.querySelector('[data-e2e="bh-more-bills"]')
-    )
-    this.log('debug', 'Exiting findMoreBillsButton')
-    return button
+    const button = document.querySelector('[data-e2e="bh-more-bills"]')
+    if (button) return true
+    else return false
   }
 
   findPdfButtons() {
