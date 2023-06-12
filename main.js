553db450fb5dfa7f65811fb8e3d3506632c02adf
Travis CI User <travis@example.org>
2023-06-12 12:44:28 +0000

    publish: Merge pull request #576 from konnectors/fix/keepConnected_selector
    generated from commit f8a9a9a8eaa13b3a90ebe763368c4872c82c1edd

--- main.js
+++ main.js
@@ -5199,9 +5199,10 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
     await Promise.race([
       this.waitForElementInWorker('#login-label'),
       this.waitForElementInWorker('#password-label'),
-      this.waitForElementInWorker('#oecs__connecte-se-deconnecter'),
+      this.waitForElementInWorker('button[data-testid="button-keepconnected"]'),
       this.waitForElementInWorker('div[class*="captcha_responseContainer"]'),
-      this.waitForElementInWorker('#undefined-label')
+      this.waitForElementInWorker('#undefined-label'),
+      this.waitForElementInWorker('#oecs__connecte-se-deconnecter')
     ])
     const { askForCaptcha, captchaUrl } = await this.runInWorker(
       'checkForCaptcha'
@@ -5211,6 +5212,7 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
       await this.waitForUserAction(captchaUrl)
     }
   }
+
   async ensureNotAuthenticated() {
     this.log('info', 'ensureNotAuthenticated starts')
     await this.navigateToLoginForm()
@@ -5315,13 +5317,17 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
         '[data-e2e="bp-tile-historic"]',
         '[aria-labelledby="bp-billsHistoryTitle"]'
       )
-      const redFrame = await this.runInWorker('checkRedFrame')
-      if (redFrame !== null) {
+      const redFrame = await this.isElementInWorker(
+        '.alert-icon icon-error-severe'
+      )
+      if (redFrame) {
         this.log('debug', 'Website did not load the bills')
         throw new Error('VENDOR_DOWN')
       }
       let recentPdfNumber = await this.runInWorker('getPdfNumber')
-      const hasMoreBills = await this.runInWorker('getMoreBillsButton')
+      const hasMoreBills = await this.isElementInWorker(
+        '[data-e2e="bh-more-bills"]'
+      )
       if (hasMoreBills) {
         await this.clickAndWait(
           '[data-e2e="bh-more-bills"]',
@@ -5334,9 +5340,11 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
         this.log('debug', 'fetching ' + (i + 1) + '/' + recentPdfNumber)
         // If something went wrong during the loading of the pdf board, a red frame with an error message appears
         // So we need to check every lap to see if we got one
-        const redFrame = await this.runInWorker('checkRedFrame')
-        if (redFrame !== null) {
-          this.log('debug', 'Something went wrong during recent pdfs loading')
+        const redFrame = await this.isElementInWorker(
+          '.alert-icon icon-error-severe'
+        )
+        if (redFrame) {
+          this.log('debug', 'Website did not load the bills')
           throw new Error('VENDOR_DOWN')
         }
         await this.runInWorker('waitForRecentPdfClicked', i)
@@ -5358,8 +5366,10 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
         for (let i = 0; i < oldPdfNumber; i++) {
           this.log('debug', 'fetching ' + (i + 1) + '/' + oldPdfNumber)
           // Same as above with the red frame, but for old bills board
-          const redFrame = await this.runInWorker('checkOldBillsRedFrame')
-          if (redFrame !== null) {
+          const redFrame = await this.isElementInWorker(
+            'span[class="alert-icon icon-error-severe"]'
+          )
+          if (redFrame) {
             this.log('debug', 'Something went wrong during old pdfs loading')
             throw new Error('VENDOR_DOWN')
           }
@@ -5455,13 +5465,6 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
     )
   }
 
-  findMoreBillsButton() {
-    this.log('debug', 'Starting findMoreBillsButton')
-    const button = document.querySelector('[data-e2e="bh-more-bills"]')
-    if (button) return true
-    else return false
-  }
-
   findPdfButtons() {
     this.log('debug', 'Starting findPdfButtons')
     const buttons = Array.from(
@@ -5487,13 +5490,16 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
   findStayLoggedButton() {
     this.log('debug', 'Starting findStayLoggedButton')
     const button = document.querySelector(
-      '[data-oevent-label="bouton_rester_identifie"]'
+      'button[data-testid="button-keepconnected"]'
     )
-    return button
+    if (button) {
+      return true
+    }
+    return false
   }
 
   findHelloMessage() {
-    this.log('debug', 'Starting findStayLoggedButton')
+    this.log('debug', 'Starting findHelloMessage')
     const messageSpan = document.querySelector(
       'span[class="d-block text-center"]'
     )
@@ -5507,7 +5513,7 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
   }
 
   findAccountPage() {
-    this.log('debug', 'Starting findLoginButton')
+    this.log('debug', 'Starting findAccountPage')
     const loginButton = document.querySelector('#oecs__connexion')
     return loginButton
   }
@@ -5555,7 +5561,7 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
       }
     }
     this.log('debug', 'found credentials, processing')
-    const askFullLogin = await this.runInWorker('checkAskFullLogin')
+    const askFullLogin = await this.isElementInWorker('#login-label')
     if (askFullLogin) {
       this.log('debug', 'askFullLogin condition')
       await this.tryAutoLogin(credentials, 'full')
@@ -5566,6 +5572,7 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
     if (credentials.email === testEmail) {
       this.log('debug', 'sameMailLogin condition')
       await this.sameMailLogin(credentials)
+      return true
     }
     if (credentials.email != testEmail) {
       this.log('debug', 'differentMailLogin condition')
@@ -5580,7 +5587,7 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
       this.log('debug', 'no credentials found but user is still logged in')
       return true
     }
-    const isAccountListPage = await this.runInWorker('checkAccountListPage')
+    const isAccountListPage = await this.isElementInWorker('#undefined-label')
     if (isAccountListPage) {
       this.log('debug', 'Webview on accountsList page, go to first login step')
       await this.runInWorker('click', '#undefined-label')
@@ -5592,9 +5599,15 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
   }
 
   async sameMailLogin(credentials) {
-    const stayLogButton = await this.runInWorker('getStayLoggedButton')
-    if (stayLogButton != null) {
-      stayLogButton.click()
+    this.log('info', 'sameMailLogin starts')
+    const stayLogButton = await this.isElementInWorker(
+      'button[data-testid="button-keepconnected"]'
+    )
+    if (stayLogButton) {
+      await this.runInWorker(
+        'click',
+        'button[data-testid="button-keepconnected"]'
+      )
       await this.waitForElementInWorker('#oecs__connecte')
       return true
     }
@@ -5646,18 +5659,9 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
   }
 
   async getLoginPage() {
-    this.log('debug', 'Starting findLoginButton')
+    this.log('debug', 'Starting getLoginPage')
     const loginButton = this.findLoginButton()
     return loginButton
-  }
-
-  async waitForFullLoading() {
-    window.addEventListener('load', () => {
-      this.log('debug', 'Page fully loaded')
-      this.log('debug', document.location.href)
-      return true
-    })
-    return false
   }
 
   async findAndSendCredentials(loginField) {
@@ -5794,18 +5798,6 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
     }
   }
 
-  async checkRedFrame() {
-    const redFrame = document.querySelector('.alert-icon icon-error-severe')
-    return redFrame
-  }
-
-  async checkOldBillsRedFrame() {
-    const redFrame = document.querySelector(
-      'span[class="alert-icon icon-error-severe"]'
-    )
-    return redFrame
-  }
-
   async getStayLoggedButton() {
     this.log('debug', 'Starting getStayLoggedButton')
     const button = this.findStayLoggedButton()
@@ -5822,12 +5814,6 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
       return result
     }
     return null
-  }
-
-  async getMoreBillsButton() {
-    this.log('debug', 'Getting in getMoreBillsButton')
-    let moreBillsButton = this.findMoreBillsButton()
-    return moreBillsButton
   }
 
   async getPdfNumber() {
@@ -5913,14 +5899,6 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
     }
   }
 
-  checkAskFullLogin() {
-    if (document.querySelector('#login-label')) {
-      return true
-    } else {
-      return false
-    }
-  }
-
   checkForCaptcha() {
     const captchaContainer = document.querySelector(
       'div[class*="captcha_responseContainer"]'
@@ -5936,17 +5914,12 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
     const passwordInput = document.querySelector('#password')
     const loginInput = document.querySelector('#login')
     const otherAccountButton = document.querySelector('#undefined-label')
-    if (passwordInput || loginInput || otherAccountButton) {
+    const stayLoggedButton = document.querySelector(
+      'button[data-testid="button-keepconnected"]'
+    )
+    if (passwordInput || loginInput || otherAccountButton || stayLoggedButton) {
       return true
     }
-    return false
-  }
-
-  async checkAccountListPage() {
-    const isAccountListPage = Boolean(
-      document.querySelector('#undefined-label')
-    )
-    if (isAccountListPage) return true
     return false
   }
 }
@@ -5958,9 +5931,6 @@ connector
       'getUserMail',
       'findClientRef',
       'processingBills',
-      'getMoreBillsButton',
-      'checkRedFrame',
-      'checkOldBillsRedFrame',
       'getTestEmail',
       'fillingForm',
       'getStayLoggedButton',
@@ -5968,7 +5938,6 @@ connector
       'getPdfNumber',
       'waitForRecentPdfClicked',
       'waitForOldPdfClicked',
-      'waitForFullLoading',
       'getLoginPage',
       'getAccountPage',
       'clickLoginPage',
@@ -5976,10 +5945,8 @@ connector
       'accountSelection',
       'getLogoutButton',
       'getIdentity',
-      'checkAskFullLogin',
       'checkForCaptcha',
-      'waitForCaptchaResolution',
-      'checkAccountListPage'
+      'waitForCaptchaResolution'
     ]
   })
   .catch(err => {
