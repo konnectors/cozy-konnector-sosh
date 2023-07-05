1f67560ec546a3a1167c0a5ea2097b588a300f1a
Travis CI User <travis@example.org>
2023-07-05 21:24:48 +0000

    publish: fix(deps): update dependency cozy-clisk to ^0.20.2
    generated from commit 6b9156a4cd053ea39de6cf5cbcb5edf917ebb221

--- main.js
+++ main.js
@@ -1474,7 +1474,6 @@ var ContentScript = /*#__PURE__*/function () {
      * Evaluates a given function in worker context
      *
      * @param {Function} fn - the function to evaluate
-     *
      * @returns {Promise<any>} - function evaluation result
      */
 
@@ -1520,8 +1519,7 @@ var ContentScript = /*#__PURE__*/function () {
     /**
      * Evaluates a given function string
      *
-     * @param {String} fnString - the function string to evaluate
-     *
+     * @param {string} fnString - the function string to evaluate
      * @returns {Promise<any>} - function evaluation result
      */
 
@@ -4545,8 +4543,7 @@ function blobToBase64(_x) {
 /**
  * Convert a string function to the corresponding function.
  *
- * @param {String} fnString - function string to convert
- *
+ * @param {string} fnString - function string to convert
  * @returns {Function} - the resulting function
  */
 
@@ -4585,8 +4582,7 @@ function deserializeStringFunction(fnString) {
 /**
  * Calls and awaits the given string function with given arguments
  *
- * @param {String} fnString - function string to convert
- *
+ * @param {string} fnString - function string to convert
  * @returns {Promise<any>} - the result of the execution of the string function
  */
 
@@ -5299,7 +5295,7 @@ module.exports = _defineProperty, module.exports.__esModule = true, module.expor
 /***/ ((module) => {
 
 "use strict";
-module.exports = JSON.parse('{"name":"cozy-clisk","version":"0.20.0","description":"All the libs needed to run a cozy client connector","repository":{"type":"git","url":"git+https://github.com/konnectors/libs.git"},"files":["dist"],"keywords":["konnector"],"main":"dist/index.js","author":"doubleface <christophe@cozycloud.cc>","license":"MIT","bugs":{"url":"https://github.com/konnectors/libs/issues"},"homepage":"https://github.com/konnectors/libs#readme","scripts":{"lint":"eslint \'src/**/*.js\'","prepublishOnly":"yarn run build","build":"babel --root-mode upward src/ -d dist/ --copy-files --verbose --ignore \'**/*.spec.js\',\'**/*.spec.jsx\'","test":"jest src"},"devDependencies":{"@babel/core":"7.20.12","babel-jest":"29.3.1","babel-preset-cozy-app":"2.0.4","jest":"29.3.1","jest-environment-jsdom":"29.3.1","typescript":"4.9.5"},"dependencies":{"@cozy/minilog":"^1.0.0","bluebird-retry":"^0.11.0","cozy-client":"^34.11.0","ky":"^0.25.1","lodash":"^4.17.21","p-wait-for":"^5.0.2","post-me":"^0.4.5"},"gitHead":"ac40716ad355c698fa9f29e6eb61eed65500b876"}');
+module.exports = JSON.parse('{"name":"cozy-clisk","version":"0.20.2","description":"All the libs needed to run a cozy client connector","repository":{"type":"git","url":"git+https://github.com/konnectors/libs.git"},"files":["dist"],"keywords":["konnector"],"main":"dist/index.js","author":"doubleface <christophe@cozycloud.cc>","license":"MIT","bugs":{"url":"https://github.com/konnectors/libs/issues"},"homepage":"https://github.com/konnectors/libs#readme","scripts":{"lint":"eslint \'src/**/*.js\'","prepublishOnly":"yarn run build","build":"babel --root-mode upward src/ -d dist/ --copy-files --verbose --ignore \'**/*.spec.js\',\'**/*.spec.jsx\'","test":"jest src"},"devDependencies":{"@babel/core":"7.20.12","babel-jest":"29.3.1","babel-preset-cozy-app":"2.0.4","jest":"29.3.1","jest-environment-jsdom":"29.3.1","typescript":"4.9.5"},"dependencies":{"@cozy/minilog":"^1.0.0","bluebird-retry":"^0.11.0","cozy-client":"^34.11.0","ky":"^0.25.1","lodash":"^4.17.21","p-wait-for":"^5.0.2","post-me":"^0.4.5"},"gitHead":"dbd76a250e5bdf99f10102bd4b979bc277ade383"}');
 
 /***/ })
 /******/ 	]);
