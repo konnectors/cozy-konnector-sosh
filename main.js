90a32a5377d52af1bdfa78aa2cf6fd9bb44dba97
git-directory-deploy <>
2025-06-18 15:08:00 +0000

    publish: Merge pull request #655 from konnectors/feat/PromiseAllSettled
    generated from commit e420725bef89e46711fe16880d02a7b6b0fdaf26

--- main.js
+++ main.js
@@ -18749,11 +18749,18 @@ class SoshContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_
       ),
       this.waitForElementInWorker(
         'a[data-e2e="btn-contact-info-phone-modifier"]'
-      ),
-      this.waitForElementInWorker(
-        'a[data-e2e="btn-contact-info-modifier-vos-adresses-postales"]'
       )
     ])
+    // Some users does not have any adresse registered on theur account, we got to treat this separatedly to avoid konnector crashing
+    try {
+      await this.waitForElementInWorker(
+        'a[data-e2e="btn-contact-info-modifier-vos-adresses-postales"]',
+        { timeout: 5000 }
+      )
+    } catch (_) {
+      // Catch it so it don't crash, no big deal if not available
+      this.log('warn', 'Address element is not present on the page')
+    }
   }
 
   async waitForUserAction(url) {
